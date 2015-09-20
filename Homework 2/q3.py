4b28fde093c89a06bc4d7768f60ee671462cf811
kyajmiller <zjm203@yahoo.com>
2015-09-20 15:03:38 -0700

    question 3 algorithm

---
+++ Homework 2/q3.py
@@ -0,0 +1,13 @@
+def get_value(x):
+    t = (-1.50718122e-02 * x) + 4.09241546e+01
+    return t
+
+def getSquaredError(predicted, actual):
+    difference = actual - predicted
+    squaredError = difference * difference
+    return squaredError
+
+prediction_2012 = get_value(2012)
+print(prediction_2012)
+squaredError_2012 = getSquaredError(prediction_2012, 10.75)
+print(squaredError_2012)
