5c5fe18f27c079f03b6a532a29d4ebd7f5d54017
kyajmiller <zjm203@yahoo.com>
2015-10-12 21:27:10 -0700

    sample scripts and data for homework 2

--- Homework 3/q1.py
+++ Homework 3/q1.py
@@ -1,6 +1,25 @@
 """
-Poisson probability mass function
+Kya Miller
+INFO 521 Homework 3
 p(y) = ((lambda^y)/(y.factorial)) * e^-lambda
+lambda = 8
 """
+import math
 
-print(2 * 1e-10)
+
+def calculateProbability(y):
+    lamb = 8
+    e = 10
+    probability = (math.pow(lamb, y) / math.factorial(y)) * math.pow(e, -lamb)
+    return probability
+
+
+probYLessThanEqualToSix = 0
+for i in range(6):
+    y = i + 1
+    probYLessThanEqualToSix += calculateProbability(y)
+
+probYGreaterThanSix = 1 - probYLessThanEqualToSix
+
+print(probYLessThanEqualToSix)
+print(probYGreaterThanSix)
