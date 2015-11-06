27917c424ac3bcd6d9bdd2e7387c11b7acbdd820
kyajmiller <zjm203@yahoo.com>
2015-11-06 14:51:49 -0700

    somehow the var is finally positive

--- Homework 4/plot_laplace_approx.py
+++ Homework 4/plot_laplace_approx.py
@@ -17,16 +17,40 @@ def plotFirstSet():
 
 
 def plotBetaPosteriorAndLaplaceApproximation(a, B, N, y):
-    r = (1 - a - y)/(1 - a - (2 * y) + B + N)
-    var = (-math.pow(r, 2) * (math.pow((1 - r), 2))) / ((math.pow((1 - r), 2) * (1 - a - y)) - (math.pow(r, 2) * (B - N + y)))
+    # r = (1 - a - y) / (-N - B - a + 2)
+    # var = (math.pow(r, 2) * math.pow((1 - r), 2)) / ((math.pow((1 - r), 2) * (1 - a - y)) + (math.pow(r, 2) * (1 - B - N + y)))
+    # r = (1 - a - y)/(1 - a - (2 * y) + B + N)
+    # var = (-math.pow(r, 2) * math.pow((1 - r), 2)) / ((math.pow((1 - r), 2) * (1 - a - y)) + (math.pow(r, 2) * (1 - B - N + y)))
+
+    r = (1 - y - a) / (2 - a - N - B)
+    var = (-math.pow(r, 2) * math.pow((1 - r), 2)) / (
+    (math.pow((1 - r), 2) * (1 - a - y)) + (math.pow(r, 2) * (1 - B - N + y)))
+    # var = (math.pow(r, 2) * math.pow((1 - r), 2)) / ((math.pow((1 - r), 2) * (y + a - 1)) + (math.pow(r, 2) * (1 - N + y - B)))
     x = numpy.linspace(0.01, 1, 1000)
 
+    print(r)
+    print(var)
+
+    plotBeta = beta.pdf(x, a, B, loc=0, scale=1)
+    plt.plot(x, plotBeta)
+    plt.show()
+
+    '''
     t = beta.pdf(x, a, B)
     plt.plot(x, t)
 
-    laplace = beta.pdf(x, a, B, loc=r, scale=var)
+    t = beta.pdf(x, a, B, loc=r, scale=math.sqrt(var))
+    plt.plot(x, t, 'r--')
+    plt.show()
+
+
+    laplace = beta.pdf(x, a, B, loc=r, scale=math.sqrt(var))
     plt.plot(x, laplace, 'r')
     plt.show()
+
+    print(math.sqrt(var))
+    print(r)
+    '''
 
 
 
