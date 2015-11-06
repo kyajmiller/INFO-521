283c3aa7c83bc0b82fb8b818be47a197b6b2df4d
kyajmiller <zjm203@yahoo.com>
2015-11-06 01:44:28 -0700

    homework 4 plot stuff for q6

---
+++ Homework 4/plot_laplace_approx.py
@@ -0,0 +1,16 @@
+__author__ = 'Kya'
+from __future__ import division
+import numpy
+import math
+import matplotlib.pyplot as plt
+
+def plotFirstSet():
+    a = 5
+    B = 5
+    N = 20
+    y = 10
+
+
+def
+r = (1 - a - y)/(1 - a - (2 * y) + B + N)
+var = (-math.pow(r, 2) * (math.pow((1 - r), 2))) / ((math.pow((1 - r), 2) * (1 - a - y)) - (math.pow(r, 2) * (B - N + y)))
