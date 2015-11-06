ebf82f72e5a85dfeb4e218743229a8f6d42ffc8d
kyajmiller <zjm203@yahoo.com>
2015-11-06 15:11:15 -0700

    plot the three things

--- Homework 4/plot_laplace_approx.py
+++ Homework 4/plot_laplace_approx.py
@@ -16,44 +16,45 @@ def plotFirstSet():
     plotBetaPosteriorAndLaplaceApproximation(a, B, N, y)
 
 
-def plotBetaPosteriorAndLaplaceApproximation(a, B, N, y):
-    # r = (1 - a - y) / (-N - B - a + 2)
-    # var = (math.pow(r, 2) * math.pow((1 - r), 2)) / ((math.pow((1 - r), 2) * (1 - a - y)) + (math.pow(r, 2) * (1 - B - N + y)))
-    # r = (1 - a - y)/(1 - a - (2 * y) + B + N)
-    # var = (-math.pow(r, 2) * math.pow((1 - r), 2)) / ((math.pow((1 - r), 2) * (1 - a - y)) + (math.pow(r, 2) * (1 - B - N + y)))
+def plotSecondSet():
+    a = 3
+    B = 15
+    N = 10
+    y = 3
 
+    plotBetaPosteriorAndLaplaceApproximation(a, B, N, y)
+
+
+def plotThirdSet():
+    a = 1
+    B = 30
+    N = 10
+    y = 3
+
+    plotBetaPosteriorAndLaplaceApproximation(a, B, N, y)
+
+
+def plotBetaPosteriorAndLaplaceApproximation(a, B, N, y):
     r = (1 - y - a) / (2 - a - N - B)
-    var = (-math.pow(r, 2) * math.pow((1 - r), 2)) / (
-    (math.pow((1 - r), 2) * (1 - a - y)) + (math.pow(r, 2) * (1 - B - N + y)))
-    # var = (math.pow(r, 2) * math.pow((1 - r), 2)) / ((math.pow((1 - r), 2) * (y + a - 1)) + (math.pow(r, 2) * (1 - N + y - B)))
+    var = -(math.pow(r, 2) * math.pow((1 - r), 2)) / (
+        (math.pow((1 - r), 2) * (1 - a - y)) + (math.pow(r, 2) * (1 - B - N + y)))
     x = numpy.linspace(0.01, 1, 1000)
 
     print(r)
     print(var)
 
-    plotBeta = beta.pdf(x, a, B, loc=0, scale=1)
-    plt.plot(x, plotBeta)
+    betaT = beta.pdf(x, a, B)
+    plt.plot(x, betaT, 'b')
+
+    normT = norm.pdf(x, loc=r, scale=math.sqrt(var))
+    plt.plot(x, normT, 'r--')
     plt.show()
 
-    '''
-    t = beta.pdf(x, a, B)
-    plt.plot(x, t)
-
-    t = beta.pdf(x, a, B, loc=r, scale=math.sqrt(var))
-    plt.plot(x, t, 'r--')
-    plt.show()
-
-
-    laplace = beta.pdf(x, a, B, loc=r, scale=math.sqrt(var))
-    plt.plot(x, laplace, 'r')
-    plt.show()
-
-    print(math.sqrt(var))
-    print(r)
-    '''
 
 
 
 
 
 plotFirstSet()
+plotSecondSet()
+plotThirdSet()
