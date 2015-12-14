907347c4dba353f056f18297231094ba1d19c036
kyajmiller <zjm203@yahoo.com>
2015-12-13 22:13:27 -0700

    reformat

--- Final Project/finalProjectUtilityFunctions.py
+++ Final Project/finalProjectUtilityFunctions.py
@@ -1,7 +1,6 @@
 def makeDataSet(dataLines):
     liberalStates = ['WA', 'OR', 'CA', 'NV', 'CO', 'NM', 'IA', 'WI', 'MI', 'IL', 'OH', 'VA', 'DC', 'MD', 'DE', 'PA',
-                     'NJ',
-                     'NY', 'CT', 'RI', 'MA', 'NH', 'VT', 'ME', 'HI', 'FL']
+                     'NJ', 'NY', 'CT', 'RI', 'MA', 'NH', 'VT', 'ME', 'HI', 'FL']
 
     conservativeStates = ['ID', 'MT', 'WY', 'UT', 'AZ', 'ND', 'SD', 'NE', 'KS', 'OK', 'TX', 'MO', 'AR', 'LA', 'IL',
                           'KY', 'TN', 'MS', 'AL', 'WV', 'NC', 'SC', 'GA', 'AK']
