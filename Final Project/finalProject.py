37e92c527624df914a7bed514dff9d6027c72022
kyajmiller <zjm203@yahoo.com>
2015-12-13 23:05:15 -0700

    build features vectors

--- Final Project/finalProject.py
+++ Final Project/finalProject.py
@@ -1,27 +1,21 @@
 from Perceptron import *
 from finalProjectUtilityFunctions import *
 from sklearn.linear_model import Perceptron
-import random
-import math
-
-
-def getDataSets():
-    # open input file
-    data = open('foodTweets.txt', 'r')
-    allData = data.readlines()
-
-    # shuffle data so get different sets each time
-    random.shuffle(allData)
-
-    # train on 80% of data, test on 20%
-    trainingSize = int(math.ceil(len(allData) * 0.8))
-    trainingSet = allData[:trainingSize]
-    testingSet = allData[trainingSize:]
-
-    trainingSet = makeDataSet(trainingSet)
-    testingSet = makeDataSet(testingSet)
-
-    return trainingSet, testingSet
-
+import pandas
+import itertools
 
 trainingSet, testingSet = getDataSets()
+
+frame = pandas.DataFrame(columns=['state', 'label', 'features'])
+
+for index, value in enumerate(testingSet):
+    frame.loc[index] = [value['state'], value['label'], value['features']]
+
+features = pandas.Series(list(itertools.chain(*[t['features'] for t in trainingSet])))
+features = features.value_counts()
+print "Number of Features: %i" % features.size
+
+# Feature Vectors
+print "Building Features Vectors..."
+X_train = makeFeaturesVectors([t['features'] for t in trainingSet], features.index)
+X_test = makeFeaturesVectors([t['features'] for t in testingSet], features.index)
