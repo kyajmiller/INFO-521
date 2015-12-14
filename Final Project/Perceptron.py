49c3c379f81900ae03fd8fe3b3b83fe8c5e4cafd
kyajmiller <zjm203@yahoo.com>
2015-12-13 21:23:30 -0700

    add function to train and predict things

--- Final Project/Perceptron.py
+++ Final Project/Perceptron.py
@@ -9,3 +9,25 @@ class Perceptron(object):
         self.learningRate = learningRate
 
         self.isTrained = False
+        self.weights = None
+
+    def train(self, trainingVectors, trainingLabels):
+        if self.isTrained:
+            print("Perceptron is already trained!")
+        else:
+            self.weights = [numpy.transpose(numpy.matrix(numpy.zeros(trainingVectors.shape[1]))) for i in
+                            xrange(self.numClasses)]
+            actualWeights = [numpy.transpose(numpy.matrix(numpy.zeros(trainingVectors.shape[1]))) for i in
+                             xrange(self.numClasses)]
+            iterations = 0
+
+            for i in xrange(self.epochs):
+                errors = 0
+                for j, label in zip(range(trainingVectors.shape[0]), trainingLabels):
+                    value = trainingVectors[j, :]
+                    prediction = self.predict(value)
+
+    def predict(self, value):
+        prediction = [numpy.dot(value, weight)[0] for weight in self.weights]
+        maxValue = prediction.index(max(prediction))
+        return maxValue
