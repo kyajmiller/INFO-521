c3d18cbf70b1d625a3030bafcdc4d53b7285c717
kyajmiller <zjm203@yahoo.com>
2015-12-13 21:31:20 -0700

    comment out everything, something died somewhere, just copy over the right one and start refactoring

--- Final Project/Perceptron.py
+++ Final Project/Perceptron.py
@@ -1,3 +1,67 @@
+from __future__ import division
+import pandas as pd
+import numpy as np
+
+
+class Perceptron:
+    ''' Implementation of the multinomial perceptron '''
+
+    def __init__(self, classes=3, epocs=10, lrate=1):
+        self.epocs = epocs
+        self.classes = classes
+        self.lrate = lrate
+        self.w = None  # The perceptron is not initialized yet
+
+    def train(self, X, t):
+        ''' Learn the weights from the training data '''
+
+        assert self.w == None, "This perceptron is already trained"
+
+        # initialize the weights
+        # self.w = [lil_matrix(X[0].shape).T for i in xrange(self.classes)]
+        self.w = [np.matrix(np.zeros(X.shape[1])).T for i in xrange(self.classes)]
+        aw = [np.matrix(np.zeros(X.shape[1])).T for i in xrange(self.classes)]
+        iterations = 0
+
+        for i in xrange(self.epocs):
+            errors = 0
+
+            for a, c in zip(range(X.shape[0]), t):
+
+                v = X[a, :]
+
+                p = self.predict(v)
+
+                if not p == c:  # If the prediction is wrong
+                    for i in xrange(self.classes):
+                        if i == p:
+                            self.w[i] -= self.lrate * v.T
+                        else:
+                            self.w[i] += self.lrate * v.T
+                    errors += 1
+
+                # Accumulate the w vectors
+                for i in range(len(aw)):
+                    aw[i] += self.w[i]
+
+                iterations += 1
+
+            if errors == 0:
+                break
+        # Perform the averaging
+        for i in range(len(aw)):
+            aw[i] /= iterations
+
+        self.w = aw
+
+    def predict(self, v):
+        ''' Multiclass prediction '''
+        p = [v.dot(w)[0] for w in self.w]
+
+        return p.index(max(p))
+
+
+'''
 import pandas
 import numpy
 
@@ -41,7 +105,18 @@ class Perceptron(object):
 
                     iterations += 1
 
+                if errors == 0:
+                    break
+
+            # average values
+            for m in range(len(adjustedWeights)):
+                adjustedWeights[m] /= iterations
+
+
+
+
     def predict(self, value):
         prediction = [numpy.dot(value, weight)[0] for weight in self.weights]
         maxValue = prediction.index(max(prediction))
         return maxValue
+'''
