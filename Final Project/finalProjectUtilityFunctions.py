82417445d23b38ccd6724263408f792b568b877f
kyajmiller <zjm203@yahoo.com>
2015-12-13 22:56:16 -0700

    move getDataSets to utility functions file

--- Final Project/finalProjectUtilityFunctions.py
+++ Final Project/finalProjectUtilityFunctions.py
@@ -1,6 +1,27 @@
 import re
 import numpy
 import pandas
+import random
+import math
+
+
+def getDataSets():
+    # open input file
+    data = open('foodTweets.txt', 'r')
+    allData = data.readlines()
+
+    # shuffle data so get different sets each time
+    random.shuffle(allData)
+
+    # train on 80% of data, test on 20%
+    trainingSize = int(math.ceil(len(allData) * 0.8))
+    trainingSet = allData[:trainingSize]
+    testingSet = allData[trainingSize:]
+
+    trainingSet = makeDataSet(trainingSet)
+    testingSet = makeDataSet(testingSet)
+
+    return trainingSet, testingSet
 
 
 def makeDataSet(dataLines):
